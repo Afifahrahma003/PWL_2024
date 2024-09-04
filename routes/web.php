129a51d90c6917e284062830c2ae6fd9f8d1da5d
Afifah Rahma <afifahhrahmaa03@gmail.com>
2024-09-04 19:37:33 +0700

    JS2 Praktukum 1-3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,82 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Praktikum 1 
+
+Route::get('/hello', function () {
+    return 'Hello World';
+   });
+   
+
+Route::get('/world', function () {
+    return 'World';
+   });
+   
+
+Route::get('/salam', function () {
+    return 'Selamat Datang';
+   });
+
+
+Route::get('/about', function () {
+    return 'Hai! Nama saya Afifah Rahma  NIM : 2241760088';
+    });
+
+
+Route::get('/user/{name}', function ($name) {
+        return 'Nama saya '.$name;
+    });   
+
+Route::get('/posts/{post}/comments/{comment}', function 
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+    });
+    
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+    });
+    
+
+Route::get('/user/{name?}', function ($name='afifah rahma') {
+    return 'Nama saya '.$name;
+    });
+
+
+
+    //Praktikum 2 Controller
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/index', [HomeController::class,'index']);
+
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
+
+//controller photo
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+  'index', 'show'
+ ]);
+ Route::resource('photos', PhotoController::class)->except([
+  'create', 'store', 'update', 'destroy'
+ ]);
+ 
+
+// //Pratikum 3
+
+Route::get('/greeting', function () {
+  return view('blog.hello', ['name' => 'Afifah']);
+  });
+
+Route::get('/greeting', [WelcomeController::class,
+'greeting']);
